9eb106184eccdf641fa7cf5e5104c82991c1e6ac
yabiku <h_yabiku@atoj.co.jp>
2018-12-07 15:56:50 +0900

    update

--- assets/js/main.ts
+++ assets/js/main.ts
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 
 var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!',
-        show: true
-    }
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    show: true
+  }
 })
 
